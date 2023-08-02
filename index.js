f8aef0a161c652863072ee5cb2c432ae5ca8d1b3
Cesar Garcia <79774422+cesare-garcia@users.noreply.github.com>
2023-08-02 18:15:18 -0500

    complete the rebalancing function

--- index.js
+++ index.js
@@ -310,11 +310,12 @@ const treeFactory = (array) => {
     const rebalance = () => {
         let newSortedArray = tree.inorder();
         level0Root = buildTree(newSortedArray);
+        return prettyPrint(level0Root);
     };
 
     prettyPrint(level0Root);
 
-    return {getRoot, insertNode, deleteNode, findNode, levelOrder, inorder, preorder, postorder, returnHeight, returnDepth, isBalanced, rebalance, prettyPrint};
+    return {getRoot, insertNode, deleteNode, findNode, levelOrder, inorder, preorder, postorder, returnHeight, returnDepth, isBalanced, rebalance};
 };
 
 // let tree = treeFactory([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
